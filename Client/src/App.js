792b7099a2b80be3e77813e87eb19eea5f3d4a10
chuleeshen <leeshen.chu@monash.edu>
2025-10-08 15:12:16 +0800

    refactor: remove login page route and link default to globe dashboard

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import GlobeDashboard from './components/GlobeDashboard';
 import Registration from './components/Registration';
@@ -12,14 +11,13 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/healthchart2013" element={<HealthChart2013 />} />
         <Route path="/healthchart2018" element={<HealthChart2018 />} />
         <Route path="/globedashboard" element={<GlobeDashboard />} />
         <Route path="/register" element={<Registration />} />
         <Route path="/page3" element={<Page3 />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<GlobeDashboard />} />
       </Routes>
     </Router>
   );
